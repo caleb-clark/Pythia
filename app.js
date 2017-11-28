5145fd5e5d1fcee2883791793526cc062c44671c
Caleb Clark <clclark@up.com>
2017-11-28 15:57:17 -0600

    update

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-var app = document.getElementById('app');
+var userText;
+var app;
 
 
-class application {
-	constructor() {
-		this.form = app.createElement('form');
-	}
+function apiTest() {
+	var api_response;
+	$.ajax({
+		'url':"https://api.textrazor.com",
+		'x-textrazor-key': "40831997fc30a0215d6072917c65a54eb0ee04cb34ff060711379a00",
+		'type': "POST",
+		'extractors': "entities, topics,entailments, relations",
+		'text': "What is the area under the curve x to the third power from 0 to 3?",
+		'async': false,
+		success: function (data) {
+			api_response = data;
+		}
+	});
+	return api_response;
+}
+
+function userSubmit() {
+
+	userText = $('#text-area').val();
+	
+	$('#text-area').val("");
+
+
+	son = apiTest();
+
+	alert(son.response);
+
+
+
+}
+
+function onload() {
+	return;
 }
 
 
-function set_up() {
-	var form = app.createElement('form');
-	var user_text = form.createElement('input');
-	user_text.type = 'text';
-}
-
-document.addEventListener("load", set_up);
 
